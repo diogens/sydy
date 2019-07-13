cafbeabc80468bafb9a6d422ff83dea17528122e
Diogenes <diogensgreen@gmail.com>
2019-07-13 17:27:47 -0300

    Updates

---
+++ precache-manifest.e39fcfcaced5c725c59c2160e690040a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43bd9786a56a8aabfd95006e77f73634",
+    "url": "/sydy/index.html"
+  },
+  {
+    "revision": "0f13448dd7e69d2810c6",
+    "url": "/sydy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sydy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0f13448dd7e69d2810c6",
+    "url": "/sydy/static/js/main.49a0dcd6.chunk.js"
+  },
+  {
+    "revision": "71207f1256dacb9daff6",
+    "url": "/sydy/static/js/runtime~main.65e0e7d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sydy/static/media/logo.5d5d9eef.svg"
+  }
+]);
